243eb67b333c6980f08093be67681c904ca4e0bc
raymond.zhuang <zhuanglm@gmail.com>
2021-10-19 11:53:41 -0400

    Initial Commit

---
+++ cpaysdkdemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cpaysdkdemo
+//
+//  Created by Raymond Zhuang on 2021-10-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cpaysdkdemo/cpaysdkdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cpaysdkdemoApp.swift
+//  cpaysdkdemo
+//
+//  Created by Raymond Zhuang on 2021-10-19.
+//
+
+import SwiftUI
+
+@main
+struct cpaysdkdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
